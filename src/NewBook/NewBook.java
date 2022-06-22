1d7afb7fbeb75d89baf84f24f645f7e22e09b298
Hakimi786 <obaidullah.hakimi@tekschool.us>
2022-06-22 08:29:47 -0700

    new book

---
+++ src/NewBook/NewBook.java
@@ -0,0 +1,15 @@
+package NewBook;
+
+public class NewBook {
+
+	public static void main(String[] args) {
+		
+		int a = 12;
+		int b = 24;
+		int c = 12+24;
+		System.out.println(c);
+		
+
+	}
+
+}
---
+++ src/NewBook/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * @author obaid
+ *
+ */
+package NewBook;
